1c55a845d0b0932294ca4a5a92a70686a3098df6
Mark Rieke <markjrieke@gmail.com>
2021-10-02 10:53:55 -0500

    script finalization

--- 2021.10.01-virginia_governors_race/scripts/va_poll_wrangle.R
+++ 2021.10.01-virginia_governors_race/scripts/va_poll_wrangle.R
@@ -4,6 +4,7 @@
 # libraries ----
 library(tidyverse)
 library(lubridate)
+library(patchwork)
 
 # themes
 source("https://raw.githubusercontent.com/markjrieke/thedatadiary/main/dd_theme_elements/dd_theme_elements.R")
@@ -34,12 +35,15 @@ polls <-
   # final cols
   select(end_date, d_votes, r_votes, t_votes)
 
+# common beta function ----
+
+
 # function for getting decayed weight ----
-poll_map <- function(inp_date) {
+poll_map <- function(inp_date, final_date) {
   
   polls %>%
     filter(end_date <= inp_date) %>%
-    mutate(days_diff = as.numeric(inp_date - end_date),
+    mutate(days_diff = as.numeric(final_date - end_date),
            d_pct = d_votes/t_votes,
            b_lower = qbeta(0.025, d_votes, r_votes),
            b_upper = qbeta(0.975, d_votes, r_votes),
@@ -61,15 +65,19 @@ poll_map <- function(inp_date) {
            r_win = 1 - pbeta(0.5, beta, alpha)) %>%
     left_join(polls, by = c("date" = "end_date")) %>%
     mutate(poll_res = d_votes/t_votes) %>%
-    select(date, poll_res, 
+    select(date, poll_res, alpha, beta,
            d_voteshare, d_lower, d_upper, d_win, 
            r_voteshare, r_lower, r_upper, r_win)  
   
 }
 
 # plot polling results & confidence bands ----
-seq(ymd("2021-06-06"), ymd("2021-11-02"), by = "days") %>%
-  map_dfr(~poll_map(.x)) %>%
+days_sequence <- seq(ymd("2021-06-06"), ymd("2021-11-02"), by = "days")
+
+expected_vote <- 
+  map2_dfr(days_sequence,
+           days_sequence,
+           ~poll_map(.x, .y)) %>%
   mutate(remove = if_else(date < Sys.Date() & is.na(poll_res), "remove", "x")) %>%
   filter(remove != "remove") %>%
   select(-remove) %>%
@@ -87,22 +95,23 @@ seq(ymd("2021-06-06"), ymd("2021-11-02"), by = "days") %>%
              linetype = "dashed",
              size = 1,
              color = dd_gray) +
-  geom_point(aes(y = poll_res,
-                 color = poll_res),
-             size = 3.5,
-             alpha = 0.8) +
-  scale_color_gradientn(colors = c(dd_red_dark, dd_red, "white", dd_blue, dd_blue_dark),
-                        values = c(0, 0.475, 0.5, 0.525, 1),
-                        limits = c(0, 1)) +
+  geom_point(aes(y = poll_res),
+             color = dd_blue,
+             size = 3,
+             alpha = 0.3) +
+  geom_point(aes(y = 1 - poll_res),
+             color = dd_red,
+             size = 3,
+             alpha = 0.3) +
   geom_line(aes(y = r_voteshare),
-            color = "white",
-            size = 3) +
-  geom_line(aes(y = d_voteshare),
             color = "white",
             size = 3) +
   geom_line(aes(y = r_voteshare),
             color = dd_red,
             size = 1.25) +
+  geom_line(aes(y = d_voteshare),
+            color = "white",
+            size = 3) +
   geom_line(aes(y = d_voteshare),
             color = dd_blue,
             size = 1.25) +
@@ -110,13 +119,73 @@ seq(ymd("2021-06-06"), ymd("2021-11-02"), by = "days") %>%
         plot.title.position = "plot",
         plot.title = element_markdown(size = 16),
         plot.subtitle = element_markdown(size = 14)) +
-  labs(title = "**Virginia Governor's Race**",
-       subtitle = "<span style=color:'#5565D7'>**McAuliffe's**</span> and <span style=color:'#D75565'>**Youngkin's**</span> two-party polling average",
+  labs(title = "**Expected Voteshare in the Virginia Governor Race**",
+       subtitle = paste("<span style=color:'#5565D7'>**McAuliffe's**</span> and <span style=color:'#D75565'>**Youngkin's**</span> two-party polling average as of",
+                        format(Sys.Date(), "%B %d"),
+                        sep = " "),
        caption = "Polls weighted by recency & sample size.\nData from @FiveThirtyEight",
        x = NULL,
        y = NULL) +
   scale_y_continuous(labels = scales::percent_format(accuracy = 1))
 
+# function to plot daily win probability ----
+
+probability <- 
+  map2_dfr(days_sequence,
+           rep(ymd("2021-11-02"), 150),
+           ~poll_map(.x, .y)) %>% 
+  ggplot(aes(x = date)) +
+  geom_hline(yintercept = 0.5,
+             linetype = "dashed",
+             size = 1,
+             color = dd_gray) +
+  geom_line(aes(y = r_win),
+            color = "white",
+            size = 3) +
+  geom_line(aes(y = r_win),
+            color = dd_red,
+            size = 1.25) +
+  geom_line(aes(y = d_win),
+            color = "white",
+            size = 3) +
+  geom_line(aes(y = d_win),
+            color = dd_blue,
+            size = 1.25) +
+  theme_minimal(base_family = "Roboto Slab") +
+  theme(plot.title.position = "plot",
+        plot.title = element_markdown(size = 16),
+        plot.subtitle = element_markdown(size = 14)) +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+  labs(title = "**Probability of Winning Virginia's Governor Race**",
+       subtitle = paste("<span style=color:'#5565D7'>**McAuliffe's**</span> and <span style=color:'#D75565'>**Youngkin's**</span> probability of winning as of",
+                        format(Sys.Date(), "%B %d"),
+                        sep = " "),
+       caption = "Polls weighted by recency & sample size.\nData from @FiveThirtyEight",
+       x = NULL,
+       y = NULL)
+
+# merge & save plots ----
+
+# patchwork plot
+expected_vote / probability
+
+# add path
+path <- "2021.10.01-virginia_governors_race/"
+
+# save with date
+ggsave(paste0(path, "plots/race_results_", Sys.Date(), ".png"),
+       width = 9,
+       height = 12,
+       units = "in",
+       dpi = 500)
+
+# save without date
+ggsave(paste0(path, "plots/race_results_current.png"),
+       width = 9,
+       height = 12,
+       units = "in",
+       dpi = 500)
+  
 
   
   
